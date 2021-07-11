4f00d496c00f35e61c6a32fa492a6f29d6f2ba38
Miecaela Garcia <76048114+mvsgarcia@users.noreply.github.com>
2021-07-11 20:10:29 +0800

    initial commit
     responsive for large devices. to be edited for medium and small devices

---
+++ Clipboard-Landing-Page_Tailwind-CSS/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  //to use jit in this project: npx tailwindcss -o ./build/tailwind.css --jit --purge "./**/*.html" -w
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        strongcyan: "hsl(171, 66%, 44%)",
+        lightblue: "hsl(233, 100%, 69%)",
+        darkgray: "hsl(210, 10%, 33%)",
+        grayblue: "hsl(201, 11%, 66%)",
+      },
+
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
